c3ae69a78a3e2abe1e0804b3c9fb84240a3af2bc
ricardo <ricardofcosta0804@gmail.com>
2025-08-25 00:15:00 -0300

    feat: import category & transactions modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,17 @@ import { CommonModule } from './common/common.module';
 import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
+import { CategoriesModule } from './categories/categories.module';
+import { TransactionsModule } from './transactions/transactions.module';
 import configuration from './config/configuration';
 
 @Module({
   imports: [AuthModule, CommonModule, UserModule,
     ConfigModule.forRoot({
       load: [configuration]
-    })
+    }),
+    CategoriesModule,
+    TransactionsModule
   ],
   controllers: [AppController],
   providers: [],
