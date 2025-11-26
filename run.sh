1f6d2b5944465a085983a6991f1f9df7e1ded8be
Ricardo Faria da Costa <ricardofcosta0804@gmail.com>
2025-11-26 00:13:16 -0300

    fixes

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 # USE ISSO NA SUA PRIMEIRA INTERAÇÃO COM O SERVIDOR
-npx prisma migrate dev --init name
+npx prisma migrate dev --name init
 # USE ISSO NAS INTERAÇÕES SEGUINTES
 # npx prisma migrate deploy
 node dist/main
